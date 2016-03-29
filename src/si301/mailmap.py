bdc1213a6c3e427ee3ac53aa5b8e2bfa0e5118af
Pat Renner <patwrenner@gmail.com>
2016-03-29 12:12:11 -0400

    added some companies, half way thru autoconf

--- src/si301/mailmap.py
+++ src/si301/mailmap.py
@@ -3,7 +3,55 @@ from si301 import email
 
 company_map = {
         "dell.com": "Dell",
-        "stanford.edu": "Stanford University"
+        "stanford.edu": "Stanford University",
+        "redhat.com": "RedHat",
+        "clisp.org": "CLISP",
+        "cs.ucla.edu": "University of California, Los Angeles",
+        "gmx.de": "GMX",
+        "h-d-gmbh.de": "H&D",
+        "aaronwl.com": "aaronwl.com" ##personal site
+        "email.unc.edu": "University of North Carolina",
+        "gnu.org": "GNU",
+        "epita.fr": "EPITA",
+        "gmail.com": "Gmail",
+        "skeeve.com": "skeeve" ## personal site
+        "air.net.au": "air.net.au",
+        "cs.stanford.edu": "Stanford University",
+        "debian.org": "debian",
+        "hawkwind.utcs.utoronto.ca": "University of Toronto",
+        "ximbiot.com": "ximbiot.com",
+        "djmnet.org": "djmnet.org",
+        "facebook.com": "Facebook",
+        "mvista.com": "Monta Vista",
+        "iluvatar.eu.org": "iluvatareu.org",
+        "byu.net": "Brigham Young University",
+        "twinsun.com": "twinsun.com",
+        "uruk.org": "uruk.org",
+        "student.uu.se": "Uppsala University",
+        "splode.com": "splode.com",
+        "interfree.it": "interfree",
+        "the-meissners.org": "the-meissners.org" ##personal site
+        "tlinx.org": "tlinx.org",
+        "dg-rtp.dg.com": "dg-rtp.dg.com",
+        "cakra.net": "cakra.net",
+        "cygnus.com": "SouthComm",
+        "google.com": "Google",
+        "dkrz.de": "DKRZ",
+        "meyering.net": "meyering.net",
+        "red-bean.com": "Red Bean Software"
+        "embecosm.com": "embecosm.com",
+        "warhol.com": "warhol.com",
+        "cisco.com": "Cisco",
+        "freefriends.org": "freefriends.org",
+        "ucw.cz": "ucw.cz",
+        "users.sourceforge.net": "users.sourceforge.net",
+        "ftbfs.org": "ftbfs.org"
+        "SLAC.Stanford.EDU": "Stanford",
+        "sim.no" : "sim.no",
+        "altlinux.org": "altlinux.org",
+        "spaceroots.org": "spaceroots.org",
+        "rexx.org": "rexx.org", ##personal
+        
         }
 
 
